acf30124e6b12a6f85a5a48a12c2dda8eaecb380
ZcR-01 <zihan1048@gmail.com>
2023-02-27 11:54:24 +0100

    models

--- eazytapes/web/models.py
+++ eazytapes/web/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. Base de dades; Controller - Python (logical business)
+# Escrit en python pero el django ho tradueix a sql
+
+class Bar(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Tapa(models.Model):
+    name = models.CharField(max_length=200)
+    bar = models.ForeignKey(Bar, on_delete=models.CASCADE) # Clau forana que apunta a bar, si es borra el bar tambe es borrara
+    #la tapa
+
